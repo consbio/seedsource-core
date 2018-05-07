78a81470661ba5dc7768181edfb67fc56789623f
Nik Molnar <nik.molnar@consbio.org>
2018-05-07 10:19:28 -0700

    Refactor javascript structure

--- javascript/src/seedsource/containers/Map.jsx
+++ javascript/src/seedsource/containers/Map.jsx
@@ -21,6 +21,15 @@ import { setPoint } from '../../actions/point'
 import { getServiceName } from '../../utils'
 import '../../leaflet-controls'
 
+/* This is a workaround for a webpack-leaflet incompatibility (https://github.com/PaulLeCam/react-leaflet/issues/255)w */
+delete L.Icon.Default.prototype._getIconUrl;
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
+  iconUrl: require('leaflet/dist/images/marker-icon.png'),
+  shadowUrl: require('leaflet/dist/images/marker-shadow.png'),
+});
+
+
 class Map extends React.Component {
     constructor(props) {
         super(props)

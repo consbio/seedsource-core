13790d911c94cf586cec5d7a370a6bf666fc124c
Bill Klinkow <klinkow@gmail.com>
2018-06-20 10:15:43 -0700

    output to directory structure

--- seedsource_core/django/seedsource/management/commands/create_vector_tiles.py
+++ seedsource_core/django/seedsource/management/commands/create_vector_tiles.py
@@ -1,23 +1,42 @@
 from django.core.management import BaseCommand
 from seedsource_core.django.seedsource.models import SeedZone
 import subprocess
+import os
+
 
 class Command(BaseCommand):
     help = 'Facilitates converting of vector data into vector tiles.'
 
+    def _create_folder(directory):
+        try:
+            if not os.path.exists(directory):
+                os.makedirs(directory)
+        except OSError:
+            print('Error: Creating directory. ' + directory)
+
+    def _write_out_yellow(output):
+        self.stdout.write('\033[0;33m' + output + '\033[0m')
+
     def handle(self, *args, **options):
-        zones = []
+        self._create_folder("seedzones")
 
-        def write_out(output):
-            self.stdout.write(output)
-
-        write_out('\nLoading data..')
         for sz in SeedZone.objects.all():
-            zones.append(sz.polygon.json)
-            write_out(sz.name)
+            FormattedName = sz.name.replace(" ", "_").replace("(", "[").replace(")", "]").replace("/", "-")
 
-        with open("geojson", "w") as f:
-            f.write('\n'.join(zones))
+            self._write_out_yellow("loading " + FormattedName)
+            with open("geojson", "w") as f:
+                f.write(sz.polygon.json)
 
-        write_out('\n\nData loaded\nLaunching process to write mbtiles..\n\n')
-        subprocess.Popen("tippecanoe -o seedtiles.mbtiles -f -zg --drop-densest-as-needed geojson", shell=True)
+            self._write_out_yellow("processing..")
+            process = subprocess.Popen(''.join([
+                "tippecanoe -o seedzones/",
+                FormattedName,
+                ".mbtiles -f -zg --drop-densest-as-needed geojson"
+                ]), shell=True)
+            ExitCode = process.wait()
+            if ExitCode == 0:
+                self.stdout.write(self.style.SUCCESS("Success\n"))
+            else:
+                self.stdout.write(self.style.ERROR("Error\n"))
+
+        self._write_out_yellow("done")

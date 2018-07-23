f41f1391135c64d48e2b4d889aa145b3298b9a8e
Nikolas Stevenson-Molnar <nik.molnar@consbio.org>
2018-07-23 16:35:10 -0700

    Refactor and add traits to `generate_scores` task

--- seedsource_core/django/seedsource/tasks/generate_scores.py
+++ seedsource_core/django/seedsource/tasks/generate_scores.py
@@ -1,21 +1,40 @@
 import numpy
+from django.conf import settings
 from ncdjango.geoprocessing.data import Raster
-from ncdjango.geoprocessing.params import ListParameter, RasterParameter, DictParameter, StringParameter
+from ncdjango.geoprocessing.evaluation import Parser, Lexer
+from ncdjango.geoprocessing.params import RasterParameter, DictParameter, StringParameter
 from ncdjango.geoprocessing.workflow import Task
+from ncdjango.models import Service
+from ncdjango.views import NetCdfDatasetMixin
 from numpy.ma import is_masked
 
 from .constraints import Constraint
 
+NC_SERVICE_DIR = settings.NC_SERVICE_DATA_ROOT
+Y_INCREASING = False
 
-class GenerateScores(Task):
+
+class GenerateScores(NetCdfDatasetMixin, Task):
     name = 'sst:generate_scores'
     inputs = [
-        ListParameter(RasterParameter(''), 'variables'),
         DictParameter('limits'),
         StringParameter('region'),
+        StringParameter('year'),
+        StringParameter('model'),
+        DictParameter('variables', required=False),
+        DictParameter('traits', required=False),
         DictParameter('constraints', required=False)
     ]
     outputs = [RasterParameter('raster_out')]
+
+    def load_variable_data(self, variable, region, year, model=None):
+        if model is not None:
+            year = '{model}_{year}'.format(model, year)
+
+        service = Service.objects.get(name='{region}_{year}Y_{variable}'.format(region, year, variable))
+        variable = service.variable_set.first()
+        data = self.get_grid_for_variable(variable)
+        return Raster(data, variable.full_extent, 1, 0, Y_INCREASING)
 
     def apply_constraints(self, data, constraints, region):
         if constraints is None:
@@ -27,43 +46,56 @@ class GenerateScores(Task):
 
         return data
 
-    def execute(self, variables, limits, region, constraints=None):
-        factors = []
+    def execute(self, limits, region, year, model, variables=[], traits=[], constraints=None):
+        data = {}
+        variable_names = {v['name'] for v in variables}
 
-        for limit in limits:
-            half = (limit['max'] - limit['min']) / 2
-            midpoint = limit['min'] + half
-            factor = 100 / half
-            mid_factor = factor * midpoint
-
-            factors.append({'factor': factor, 'mid_factor': mid_factor})
+        for trait in traits:
+            fn = trait['fn']
+            names = Lexer().get_names(fn)
+            context = {k: self.load_variable_data(k, region, year, model) for k, v in names}
+            data[trait['name']] = Parser().evaluate(fn, context)
+            data.update({k: v for k, v in context if k in variable_names})
 
         sum_rasters = None
         sum_masks = None
 
-        for i, data in enumerate(variables):
-            data = self.apply_constraints(data, constraints, region)
-            extent = data.extent
-            mask = data.mask if is_masked(data) else numpy.zeros_like(data, 'bool')
+        for item in variables + traits:
+            limit = item['limit']
+            limit_min = limit['min']
+            limit_max = limit['max']
+            half = (limit_max - limit_min) / 2
+            midpoint = limit_min + half
+            factor = 100 / half
+            mid_factor = factor * midpoint
+            
+            if item.name in data:
+                raster = data[item.name]
+                del data[item.name]
+            else:
+                raster = self.load_variable_data(item.name, region, year, model)
+            
+            extent = raster.extent
+            mask = raster.mask if is_masked(raster) else numpy.zeros_like(raster, 'bool')
 
-            mask |= data < limits[i]['min']
-            mask |= data > limits[i]['max']
-
+            mask |= raster < limit_min
+            mask |= raster > limit_min
+            
             if sum_masks is not None:
                 sum_masks |= mask
             else:
                 sum_masks = mask
 
-            data = data.view(numpy.ndarray).astype('float32')
-            data *= factors[i]['factor']
-            data -= factors[i]['mid_factor']
-            data **= 2
-            data = numpy.floor(data, data)
+            raster = raster.view(numpy.ndarray).astype('float32')
+            raster *= factor
+            raster -= mid_factor
+            raster **= 2
+            raster = numpy.floor(raster, raster)
 
             if sum_rasters is not None:
-                sum_rasters += data
+                sum_rasters += raster
             else:
-                sum_rasters = data
+                sum_rasters = raster
 
         sum_rasters += 0.4
         sum_rasters **= 0.5
@@ -73,9 +105,4 @@ class GenerateScores(Task):
         sum_rasters = 100 - sum_rasters.astype('int8')
         sum_rasters.fill_value = -128
 
-        raster = variables[0]
-        return Raster(sum_rasters, extent, raster.x_dim, raster.y_dim, raster.y_increasing)
-
-
-
-
+        return Raster(sum_rasters, extent, 1, 0, Y_INCREASING)

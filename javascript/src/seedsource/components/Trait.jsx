7dbf0373a53e3d78e770dbd77185f8e30e535466
Nikolas Stevenson-Molnar <nik.molnar@consbio.org>
2018-10-31 20:45:02 -0700

    Add trait units

--- javascript/src/seedsource/components/Trait.jsx
+++ javascript/src/seedsource/components/Trait.jsx
@@ -4,7 +4,7 @@ import ReactTooltip from "react-tooltip"
 
 export default ({ trait, traitConfig, onRemove, onTransferChange }) => {
     let { name, value } = trait
-    let { customTransfer, label, description = null } = traitConfig
+    let { customTransfer, label, units = null, description = null } = traitConfig
     let transfer = trait.transfer === null ? traitConfig.transfer : trait.transfer
 
     return <tr data-tip data-for={name + "_Tooltip"}>
@@ -22,24 +22,24 @@ export default ({ trait, traitConfig, onRemove, onTransferChange }) => {
             <strong>{label}</strong>
         </td>
         <td>
-            {value !== null ? value.toFixed(2) : '--'}
+            {value !== null ? value.toFixed(2) : '--'} {units}
         </td>
         <td>
             {
                 customTransfer ?
                 <EditableLabel value={transfer} onChange={value => onTransferChange(value)}/> : transfer
-            }
+            } {units}
 
             <ReactTooltip id={name + "_Tooltip"} className="variable-tooltip" place="right" effect="solid">
                 <h5 className="title is-5 margin-bottom-5">{label}</h5>
                 {description !== null ? <div className="is-size-7 has-text-grey-lighter">{description}</div> : null}
                 <div>
                     <span className="tooltip-label">Value at point:</span>
-                    <strong>{value !== null ? value.toFixed(2) : '--'}</strong>
+                    <strong>{value !== null ? value.toFixed(2) : '--'} {units}</strong>
                 </div>
                 <div>
                     <span className="tooltip-label">Transfer limit (+/-):</span>
-                    <strong>{transfer}</strong>
+                    <strong>{transfer} {units}</strong>
                 </div>
             </ReactTooltip>
         </td>
